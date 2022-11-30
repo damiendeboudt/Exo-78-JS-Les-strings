c2d27aaf8fcd46a2ae170a52a277fb32101a5828
Damien <damien.deboudt@sfr.fr>
2022-11-30 14:49:04 +0100

    ok

--- exercice1.js
+++ exercice1.js
@@ -2,13 +2,22 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let position = monTexte.indexOf("i");
+document.getElementById("position").innerHTML = position;
 
+let myVar = monTexte.toUpperCase();
+document.getElementById("chaineMaj").innerHTML = myVar;
 
+for (i = 0; i <monTexte.length; i++) {
+        if (monTexte.includes("a")) {
+            alert("lettre a trouvée")
+        }
+}
 
 
 
